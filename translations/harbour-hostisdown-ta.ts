c9e2580794188afca1f725639af54c155586f459
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-02 12:17:06 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: Harbour HostIsDown/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-hostisdown/translations/ta/

--- translations/harbour-hostisdown-ta.ts
+++ translations/harbour-hostisdown-ta.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="39"/>
         <source>is UP!</source>
-        <translation type="unfinished"></translation>
+        <translation>அப்!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="43"/>
         <source>is UNKNOWN!</source>
-        <translation type="unfinished"></translation>
+        <translation>தெரியவில்லை!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="48"/>
         <source>is DOWN!</source>
-        <translation type="unfinished"></translation>
+        <translation>கீழே உள்ளது!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="69"/>
         <source>Ping It!</source>
-        <translation type="unfinished"></translation>
+        <translation>அதை பிங்!</translation>
     </message>
 </context>
 <context>
@@ -29,32 +29,32 @@
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="67"/>
         <source>It was UP</source>
-        <translation type="unfinished"></translation>
+        <translation>அது மேலே இருந்தது</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="68"/>
         <source>It was DOWN</source>
-        <translation type="unfinished"></translation>
+        <translation>அது கீழே இருந்தது</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="70"/>
         <source>It was UNKNOWN</source>
-        <translation type="unfinished"></translation>
+        <translation>அது தெரியவில்லை</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="76"/>
         <source>Forget</source>
-        <translation type="unfinished"></translation>
+        <translation>மறந்துவிடு</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="81"/>
         <source>Open SSH</source>
-        <translation type="unfinished"></translation>
+        <translation>திறந்த பாஓடு</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="92"/>
         <source>Deleting</source>
-        <translation type="unfinished"></translation>
+        <translation>நீக்குதல்</translation>
     </message>
 </context>
 <context>
@@ -62,57 +62,57 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="46"/>
         <source>The host is UP!</source>
-        <translation type="unfinished"></translation>
+        <translation>புரவலன் மேலே உள்ளது!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="50"/>
         <source>The host is UNKNOWN!</source>
-        <translation type="unfinished"></translation>
+        <translation>புரவலன் தெரியவில்லை!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="55"/>
         <source>The host is DOWN!</source>
-        <translation type="unfinished"></translation>
+        <translation>புரவலன் கீழே உள்ளது!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="66"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>அமைப்புகள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="73"/>
         <source>Ping all</source>
-        <translation type="unfinished"></translation>
+        <translation>அனைத்து பிங்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="86"/>
         <source>Clear history</source>
-        <translation type="unfinished"></translation>
+        <translation>வரலாற்றை அழிக்கவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="104"/>
         <source>Ping any host!</source>
-        <translation type="unfinished"></translation>
+        <translation>எந்த ஓச்டையும் பிங்!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>An IP address or hostname</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒரு ஐபி முகவரி அல்லது ஓச்ட்பெயர்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="133"/>
         <source>Use IPv6</source>
-        <translation type="unfinished"></translation>
+        <translation>IPv6 ஐப் பயன்படுத்தவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="149"/>
         <source>Ping it</source>
-        <translation type="unfinished"></translation>
+        <translation>அதை பிங்</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="157"/>
         <source>History</source>
-        <translation type="unfinished"></translation>
+        <translation>வரலாறு</translation>
     </message>
 </context>
 <context>
@@ -120,22 +120,22 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="36"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>அமைப்புகள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="42"/>
         <source>Enable poller</source>
-        <translation type="unfinished"></translation>
+        <translation>பொல்லார்ட்டை இயக்கு</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="43"/>
         <source>Pings every N minutes all hosts in the history</source>
-        <translation type="unfinished"></translation>
+        <translation>ஒவ்வொரு n நிமிடங்களும் வரலாற்றில் அனைத்து ஓச்ட்களையும் பிங் செய்கின்றன</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="51"/>
         <source>Poller interval in minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>நிமிடங்களில் பொல்லர் இடைவெளி</translation>
     </message>
 </context>
 </TS>
